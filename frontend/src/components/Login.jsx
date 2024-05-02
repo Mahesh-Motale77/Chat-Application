6f789610d3a8587da7134811584fcc47f6b1d344
Mahesh-Motale77 <motalemahesh77@gmail.com>
2024-05-03 00:05:09 +0530

    Login frontend code

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div>
-      login
+    <div className='min-w-96 mx-auto'>
+      <div className='w-full p-6 shadow-md rounded-lg  bg-gray-500 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-15'>
+            <h1 className='text-3xl font-bold text-center'>Login</h1>
+            <form action="">
+                <div className=''>
+                    <label className='label p-2'>
+                        <span className='text-base label-text'>Username: </span>
+                    </label>
+                    <input type="text" placeholder='Enter Username' className='w-full input input-bordered h-10'/>
+                </div>
+                <div className=''>
+                    <label className='label p-2'>
+                        <span className='text-base label-text'>Password: </span>
+                    </label>
+                    <input type="password" placeholder='Enter Password' className='w-full input input-bordered h-10'/>
+                </div>
+                <p className='text-center mt-4'>Don't have an account? <Link to="/register">Signup</Link> </p>
+                <button className='btn btn-block btn-sm mt-2 border border-slate-700'>Login</button>
+                
+            </form>
+      </div>
     </div>
   )
 }
